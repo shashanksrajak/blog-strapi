ca49db8801651dd9c8ac1850727402868464a7e3
shashanksrajak <shashank.rajak@outlook.com>
2021-11-28 16:19:47 +0530

    cool

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ca44d530089e110af2c6cebe222380ad'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '97a80a94-9f84-4ae4-9608-d79ba6ae27a5'
+};
